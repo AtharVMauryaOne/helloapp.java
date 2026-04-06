bef8906b5ca010d95db8decabcc7e2ac349635fe
AtharVMauryaOne <harshmaurya045@gmail.com>
2026-04-06 14:29:57 +0530

    Update helloapp.java

--- helloapp.java
+++ helloapp.java
@@ -1,5 +1,15 @@
-public class HelloWorld {
+public class HelloApp {
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        // If no arguments
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } else {
+            // Enhanced for loop
+            for (String name : args) {
+                System.out.println("Hello " + name);
+            }
+        }
     }
 }
