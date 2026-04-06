2399713085159512eb087dac439c4acf0df0e4be
AtharVMauryaOne <harshmaurya045@gmail.com>
2026-04-06 14:31:23 +0530

    Update helloapp.java

--- helloapp.java
+++ helloapp.java
@@ -1,5 +1,23 @@
-public class HelloWorld {
+public class HelloApp {
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        // If no arguments
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } else {
+
+            String result = "";
+
+            // Build string with comma
+            for (String name : args) {
+                result += name + ", ";
+            }
+
+            // Remove last comma and space
+            result = result.substring(0, result.length() - 2);
+
+            System.out.println("Hello " + result);
+        }
     }
 }
