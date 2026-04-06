3baf85fe00d91bfd5f99c42b70f6560d6e4d84a8
AtharVMauryaOne <harshmaurya045@gmail.com>
2026-04-06 14:33:18 +0530

    Update helloapp.java

--- helloapp.java
+++ helloapp.java
@@ -1,5 +1,15 @@
-public class HelloWorld {
+public class HelloApp {
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        // If no arguments
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } else {
+            // Join all names with comma
+            String result = String.join(", ", args);
+
+            System.out.println("Hello " + result);
+        }
     }
 }
